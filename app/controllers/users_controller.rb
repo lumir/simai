3708efdb5ecbb6905f38166424ee96311757b5e7
Lumir Olivares <lumir.olivares@koombea.com>
2011-06-13 15:14:52 -0500

    edit user feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
     before_filter :authenticate_user!
     def index
+      @users = User.all
     end
 
     def new
       @user = User.new(:confirmation => "")
+    end
+
+    def show
+      @user = User.find(params[:id])
     end
 
     def create
@@ -19,6 +24,24 @@ class UsersController < ApplicationController
         flash[:error] = "Error adding the user: #{@user.errors.full_messages.to_sentence}" unless @user.errors.blank?
         flash[:error] = "Error adding the user: Validation password error" if @user.errors.blank?
         render :action => 'new'
+      end
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      @user.username = params[:user][:username]
+      val_conf = params[:user][:password] == params[:confirmation] ? true :  false
+      if @user.update_attributes(params[:user]) and val_conf
+        flash[:notice] = "User updated successfully"
+        redirect_to :controller => "home", :action => "index"
+      else
+        flash[:error] = "Error adding the user: #{@user.errors.full_messages.to_sentence}" unless @user.errors.blank?
+        flash[:error] = "Error adding the user: Validation password error" if @user.errors.blank?
+        render :action => 'edit'
       end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Simai::Application.routes.draw do
   devise_for :users
   root :to => "home#index"
+  match "/users/index" => "users#index", :as => :index_user
+  match "/users/show" => "users#show", :as => :show_user
   match "/users/create" => "users#create", :as => :create_user
   match "/users/new" => "users#new", :as => :new_user
+  match "/users/:id/edit" => "users#edit", :as => :edit_user
+  match "/users/:id/update" => "users#update", :as => :update_user
 end
